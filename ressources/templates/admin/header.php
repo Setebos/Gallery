fbea769f7b3e723541b93b8d2f286183f7a0987f
clairezed <clairezuliani@gmail.com>
2013-11-20 16:43:29 +0100

    design slideshow en cours, inclusion glyphicon

--- ressources/templates/admin/header.php
+++ ressources/templates/admin/header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <title>Test Gallery js - Espace admin </title>
         <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css"/> 
         <link rel="stylesheet" type="text/css" href="../../css/admin.css"/>
