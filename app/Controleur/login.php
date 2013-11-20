e989365ebfc703d39749d8743bf95591e80d9334
clairezed <clairezuliani@gmail.com>
2013-11-20 19:41:54 +0100

    #1 authentification encore crasseuse mais fonctionnelle quand bon login

---
+++ app/Controleur/login.php
@@ -0,0 +1,29 @@
+
+    	<?php
+    	// connection bdd
+		try {
+		   $db = new PDO($config["db"]["dbengine"].':host='.$config["db"]["host"].';dbname='.$config["db"]["dbname"], $config["db"]["username"], $config["db"]["password"]);
+		}
+		catch (Exception $e) {
+		      die('Erreur : ' . $e->getMessage());
+		}
+
+		// Authentification
+		$login = $_POST['login'];
+		$password = $_POST['password'];
+		$req = $bdd->prepare('SELECT id FROM user WHERE login = :login AND password = :password');
+		$req->execute(array(
+		    'login' => $login,
+		    'password' => $password)
+		) or die(print_r($req->errorInfo()));
+		 
+		$resultat = $req->fetch();
+		 
+		if (!$resultat) {
+		    echo 'Mauvais identifiant ou mot de passe !';
+		}
+		else {
+		    session_start();
+		    $_SESSION['id'] = $resultat['id'];
+		    $_SESSION['login'] = $login;
+    ?>
--- app/Vue/admin/index.php
+++ app/Vue/admin/index.php
@@ -1,29 +1,8 @@
 <?php
       require_once("../../../ressources/templates/admin/header.php");    
+      include("../../../Controleur/login.php");
 ?>
 
-<?php
-		// Authentification
-			$login = $_POST['login'];
-			$password = $_POST['password'];
-			$req = $bdd->prepare('SELECT id FROM user WHERE login = :login AND password = :password');
-			$req->execute(array(
-			    'login' => $login,
-			    'password' => $password)
-			) or die(print_r($req->errorInfo()));
-			 
-			$resultat = $req->fetch();
-			 
-			if (!$resultat) {
-			    echo 'Mauvais identifiant ou mot de passe !';
-			}
-			else {
-			    session_start();
-			    $_SESSION['id'] = $resultat['id'];
-			    $_SESSION['login'] = $login;
-	    // Affichage des fonctionnalités admin
-			}
-	    ?>
 		    <div class="row height-full">
 			  <div class="col-md-3 gallery-part">
 			  	<div class="gallery-header">
@@ -31,20 +10,16 @@
 			  			<a href="addGallery.php">Ajouter galerie</a>
 			  		</div>
 			  		<h3>Galeries</h3>
-			  		
-			  	</div>
-			  </div>
-			  <div class="col-md-9 picture-part">
+			 	</div>
+			 </div>
+			 <div class="col-md-9 picture-part">
 			  	<div class="picture-header">
 			  		<div class="btn btn-default pull-right">
 			  			<a href="addPicture.php">Ajouter image</a>
 			  		</div>
 			  		<h3>Images</h3>
-			  		
 			  	</div>
-			  </div>
-			</div>
-
-	
+			 </div>
+		</div>
 	</body>
 </html>
