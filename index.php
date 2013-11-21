e7d18fe728148906135c4143addcb4afb9b092df
Setebos <moffjerjerrod@gmail.com>
2013-11-21 08:40:53 +0100

    fix index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-var_dump($_SESSION);
+// var_dump($_SESSION);
 include('app/modele/connect_db.php');
 
 if  (!isset($_GET['section']) OR $_GET['section'] == 'index')
@@ -7,17 +7,17 @@ if  (!isset($_GET['section']) OR $_GET['section'] == 'index')
   include_once("app/vue/home.php");
 }
 
-if  ($_GET['section'] == 'login')
+if  (isset($_GET['section']) AND $_GET['section'] == 'login')
 {                    
   include_once("app/vue/login.php");
 }
 
-if  ($_GET['section'] == 'login_controller')
+if  (isset($_GET['section']) AND $_GET['section'] == 'login_controller')
 {                    
   include_once("app/controleur/login_controller.php");
 }
 
-if  ($_GET['section'] == 'logout_controller')
+if  (isset($_GET['section']) AND $_GET['section'] == 'logout_controller')
 {                
   include_once("app/controleur/logout_controller.php");
 }
