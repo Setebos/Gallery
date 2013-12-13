9bca4372897e066b7993a21f24d5c01b6ffad371
Setebos <moffjerjerrod@gmail.com>
2013-12-13 18:27:36 +0100

    local jquery-ui

--- ressources/templates/admin/header.php
+++ ressources/templates/admin/header.php
@@ -7,10 +7,10 @@
         <link rel="stylesheet" type="text/css" href="app/css/bootstrap.min.css"/> 
         <link rel="stylesheet" type="text/css" href="app/css/admin.css"/>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
         <script type="text/javascript" src="app/js/bootstrap.min.js"/></script>
         <script type="text/javascript" src="app/js/respond.js"></script>
+        <script type="text/javascript" src="app/js/jquery-ui.min.js"></script>
         <!--[if IE]>
             <script src="app/js/html5-ie.js"></script>
             <script src="app/js/html5shiv.js"></script>
