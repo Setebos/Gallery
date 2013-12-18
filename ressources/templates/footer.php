809ea4c427bf14fddf6c5b3351fcb348b32fb2a7
clairezed <clairezuliani@gmail.com>
2013-12-18 18:57:15 +0100

    debug pb jquery sous firefox avec tri categories

---
+++ ressources/templates/footer.php
@@ -0,0 +1,17 @@
+<footer>
+<div class="row">
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <h5> Credits </h5>
+        <p>Arrow (from lightbox) designed by <a href="http://www.thenounproject.com/jamison">Jamison Wieser</a> from the <a href="http://www.thenounproject.com">Noun Project</a><p>
+            <p> Arrow (from standard gallery navigation) designed by <a href="http://www.thenounproject.com/JuanGarces">Juan Garces</a> from the <a href="http://www.thenounproject.com">Noun Project</a><p>
+    </div>
+
+</footer>
+
+
+</body>
+</html>
--- ressources/templates/header.php
+++ ressources/templates/header.php
@@ -7,9 +7,15 @@
         <link rel="stylesheet" type="text/css" href="app/css/bootstrap.min.css"/> 
         <link rel="stylesheet" type="text/css" href="app/css/public.css"/>
         <link rel="stylesheet" type="text/css" href="app/css/plugin.css"/>
+        
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script type="text/javascript" src="app/js/jquery-ui.min.js"></script>
+        <?php $IE8 = (ereg('MSIE 8',$_SERVER['HTTP_USER_AGENT'])) ? true : false;
+            if ($IE8 == 1) {  ?>
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+           <?php  } else { ?>
+                <script type="text/javascript" src="app/js/jquery_2.0.3.min.js"></script>
+            <?php } ?>
+        <script type="text/javascript" src="app/js/jquery-ui_1.10.3.min.js"></script>
         <script type="text/javascript" src="app/js/public.js"/></script>
         <script type="text/javascript" src="app/js/jquery.slideshowTrebouzul.js"/></script>
         <script type="text/javascript" src="app/js/bootstrap.min.js"/></script>
