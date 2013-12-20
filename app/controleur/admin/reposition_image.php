731d0415b3ddc7edf1dfb060ff7797b2cf2a363d
Setebos <moffjerjerrod@gmail.com>
2013-12-20 14:35:06 +0100

    Ajout de commentaires

--- app/controleur/admin/reposition_image.php
+++ app/controleur/admin/reposition_image.php
@@ -6,8 +6,6 @@ include_once("app/modele/Image.php");
 $imageManager = new ImageManager($db);
 
 if ( count($_POST) && is_array($_POST['item']) ) {
-
-
 	foreach ( $_POST['item'] as $key => $val ) {
 		$key = $key + 1;
 		$image = $imageManager->getImage($val);
